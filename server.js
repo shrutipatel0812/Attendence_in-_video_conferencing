6b2a6c91c91a61ed79b7bea8c7c0bcd30bbe3905
shruti patel <54548457+shrutipatel0812@users.noreply.github.com>
2021-03-01 23:12:52 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,172 @@
+require('dotenv').config();
+const express = require('express')
+const app = express()
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
+const { v4: uuidV4 } = require('uuid')
+const bodyParser  = require("body-parser")
+const mongoose    = require("mongoose")
+const User        = require("./models/user")
+const bcrypt      = require('bcrypt')
+const session     = require('express-session')
+const flash       = require('connect-flash')
+
+app.set('view engine', 'ejs')
+
+//=====================================================
+
+//database connection
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true , useUnifiedTopology: true }
+);
+const connection = mongoose.connection;
+connection.once('open', () => {
+  console.log("MongoDB database connection established successfully");
+})
+//===================================================
+
+app.use(express.urlencoded({extended: true}));
+app.use(session({ secret : 'notgoodsecret'}))
+app.use(flash());
+app.use(express.static('public'))
+//app.use(express.static(__dirname + "/public"));
+
+app.use(function(req, res, next){
+  res.locals.currentUser = req.session.username;
+  res.locals.error = req.flash("error");
+  res.locals.success = req.flash("success");
+  next();
+});
+//=====================================================
+
+app.use(function(req, res, next){
+  res.locals.currentUser = req.session.username;
+  res.locals.error = req.flash("error");
+  res.locals.success = req.flash("success");
+  next();
+});
+//=====================================================
+
+const isLogin = (req , res , next)=>{
+  //console.log("REQ.USER..." , req.session.username)
+  if(!req.session.user_id){
+      req.flash('error' , 'You must be signed in first')
+      return res.redirect('/login')
+  }
+  next();
+}
+
+//=====================================================
+app.get('/' , (req ,res) =>{
+    
+  res.render('landing');
+})
+
+//==========================================================
+
+app.get('/register' , (req ,res) =>{
+  res.render('register')
+})
+app.post('/register' , async(req ,res) =>{
+  const {password , username} =req.body;
+  const hash = await bcrypt.hash(password , 12);
+  const user = new User({
+      username,
+      password: hash
+  })
+  await user.save();
+  req.session.user_id = user._id;
+  req.session.username = username;
+  
+  res.redirect('/');
+  
+})
+
+app.get('/login' , (req ,res) =>{
+  res.render('login')
+})
+
+app.post('/login' ,async (req ,res) =>{
+  const { username , password } = req.body;
+  const user = await User.findOne({username});
+  const validPassword = await bcrypt.compare(password ,user.password);
+  if(validPassword){
+      req.session.user_id = user._id;
+      req.session.username = username;
+      req.flash('success' ,'succcfully login');
+      res.redirect('/')
+  }else{
+      res.redirect('/login')
+  }
+})
+
+
+app.get('/logout' , (req ,res) =>{
+  //req.session.user_id =null;
+  req.session.destroy();
+  res.redirect('/login');
+})
+//=========================================================
+
+const exportUsersToExcel = require('./exportService');
+
+const users = [
+
+];
+
+const workSheetColumnName = [
+    
+    "Name"
+   
+]
+const workSheetName = 'Users';
+const filePath = './outputFiles/excel-from-js.xlsx';
+
+//========================================================
+
+
+
+//=========================================================
+
+app.get('/joinroom', (req, res) => {
+  res.render('Room/joinRoom', { userName: req.session.username })
+})
+app.post('/joinroom',(req, res) => {
+  const {roomId} =req.body;
+  var data={
+    
+    name: req.session.username
+}
+users.push(data);
+console.log(users);
+console.log(data);
+res.redirect("/"+roomId)
+})
+
+app.get('/createroom', (req, res) => {
+ host= req.session.username;
+  res.redirect(`/${uuidV4()}`)
+})
+
+
+app.get('/:room', (req, res) => {
+
+  res.render('room', { roomId: req.params.room })
+})
+
+io.on('connection', socket => {
+  socket.on('join-room', (roomId, userId) => {
+    socket.join(roomId)
+    socket.to(roomId).broadcast.emit('user-connected', userId)
+
+    socket.on('disconnect', () => {
+      exportUsersToExcel(users, workSheetColumnName, workSheetName, filePath);
+      socket.to(roomId).broadcast.emit('user-disconnected', userId)
+    })
+  })
+})
+//===================================================
+
+
+//======================================================
+server.listen(3000)
